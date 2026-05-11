fa498cb30e048626d5c0b0a31668e38f8e4436de
bitranox <rnowotny1966@gmail.com>
2026-05-11 14:33:24 +0200

    1connect

--- 1connect.sh
+++ 1connect.sh
@@ -1,3 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -Eeu -o pipefail
+
+ALLOWED_HOST="lxc-pydev"
+
+log_info() {
+  printf '%s [INFO] %s\n' "$(date '+%Y-%m-%d %H:%M:%S')" "$*" >&2
+}
+
+CURRENT_HOST="$(hostname -s)"
+if [[ "$CURRENT_HOST" != "$ALLOWED_HOST" ]]; then
+  log_info "Refusing to run on '${CURRENT_HOST}': this script must be run from '${ALLOWED_HOST}'."
+  exit 1
+fi
+
 host=$(basename "$(dirname "$(readlink -f "$0")")")
 exec ssh root@"$host" -i /home/srvadmin/.ssh/root@anyhost_nopass.key
